7e52fac92bf8d24d6a4f6e4d09836de57df7ceee
tomkeane07 <tomkeane07@gmail.com>
2021-01-28 12:51:19 +0000

    author controller class created with create method & add author test

---
+++ app/Http/Controllers/AuthorsController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Author;
+
+class AuthorsController extends Controller
+{
+    public function store()
+    {
+        Author::create(
+           request()->only([
+               'name', 'dob'
+           ]));
+    }
+}
---
+++ tests/Feature/AuthorManagementTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Author;
+use Carbon\Carbon;
+
+class AuthorManagementTest extends TestCase
+{
+    use RefreshDatabase;
+    /** @test */
+    public function an_author_can_be_created()
+    {
+        $this->withoutExceptionHandling();
+        $this->post('/author', [
+            'name' => 'Homer Simpson',
+            'dob' => '05/03/1960',
+        ]);
+
+        $author = Author::all();
+
+        $this->assertCount(1, $author);
+        $this->assertInstanceOf(Carbon::class, $author->first()->dob);
+        $this->assertEquals('05/03/1960', $author->first()->dob->format('m/d/Y'));
+    }
+}
