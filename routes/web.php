7f43cabcf701ff2630a0db6202e3921f44eb7736
tomkeane07 <tomkeane07@gmail.com>
2021-01-28 12:49:18 +0000

    books & author routes

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('/books', 'BooksController@store');
-Route::patch('/books/{book}', 'BooksController@update');
+Route::patch('/books/{book}-{slug}', 'BooksController@update');
+Route::delete('/books/{book}-{slug}', 'BooksController@destroy');
 
+Route::post('/author', 'AuthorsController@store');
